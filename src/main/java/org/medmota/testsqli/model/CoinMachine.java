ee297c58af6dbc8d0d478d0b4dda55bfe3e7de88
motaouia <med.motaouia@gmail.com>
2022-11-26 16:48:22 +0100

    initial-commit

--- src/main/java/org/medmota/testsqli/model/CoinMachine.java
+++ src/main/java/org/medmota/testsqli/model/CoinMachine.java
@@ -8,15 +8,15 @@ import java.util.Set;
 public class CoinMachine {
 
 	double total = 0.0;
-	Map<Integer, Integer> myMap = new HashMap<>();
+	Map<Integer, Integer> mapOfCoins = new HashMap<>();
 	
 	public void putMoneyInside(String input) {
 		String[] facteurs = input.split("x");
-		Integer keyValue = Integer.parseInt(facteurs[1]);
+		Integer coin = Integer.parseInt(facteurs[1]);
 
-		this.myMap.put(keyValue, Integer.parseInt(facteurs[0]));
+		this.mapOfCoins.put(coin, Integer.parseInt(facteurs[0]));
 
-		this.total += keyValue * this.myMap.get(keyValue);
+		this.total += coin * this.mapOfCoins.get(coin);
 	}
 
 	public double totalMoney() {
@@ -24,20 +24,20 @@ public class CoinMachine {
 	}
 
 	public Integer check(int value) {
-		return this.myMap.get(value);
+		return this.mapOfCoins.get(value);
 	}
 
 	public void getMoney(int value) {
-		Set<Integer> keySet = ((HashMap<Integer, Integer>) this.myMap).keySet();
-		Integer[] myKeysAsArrays =  keySet.toArray(new Integer[keySet.size()]);
-		Arrays.sort(myKeysAsArrays);
-		int indexOfmaxCoin = myKeysAsArrays.length - 1;
-		while (value >= myKeysAsArrays[0]) {
-			Integer maxCoin = myKeysAsArrays[indexOfmaxCoin];
+		Set<Integer> keySet = ((HashMap<Integer, Integer>) this.mapOfCoins).keySet();
+		Integer[] arrayOfCoins =  keySet.toArray(new Integer[keySet.size()]);
+		Arrays.sort(arrayOfCoins);
+		int indexOfmaxCoin = arrayOfCoins.length - 1;
+		while (value >= arrayOfCoins[0]) {
+			Integer maxCoin = arrayOfCoins[indexOfmaxCoin];
 			if (value >= maxCoin) {
 				value -= maxCoin;
-				this.myMap.put(maxCoin, (this.myMap).get(maxCoin) - 1);
-			} else if (value < myKeysAsArrays[indexOfmaxCoin]) {
+				this.mapOfCoins.put(maxCoin, (this.mapOfCoins).get(maxCoin) - 1);
+			} else if (value < arrayOfCoins[indexOfmaxCoin]) {
 				indexOfmaxCoin--;
 				continue;
 			}
